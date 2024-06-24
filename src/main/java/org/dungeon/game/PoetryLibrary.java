cf24e7474112b989189048ae3732ed8619ab43c5
FRCRUADB <FRCRUADB@KMECLTqpopjNEHZ.kme.intern>
2024-06-24 16:14:36 +0200

    (v0.0.6) Log a warning if an incomplete poem is found

--- src/main/java/org/dungeon/game/PoetryLibrary.java
+++ src/main/java/org/dungeon/game/PoetryLibrary.java
@@ -25,6 +25,7 @@ public final class PoetryLibrary extends Library{
 	void load() {
 		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 		ResourceReader reader = new ResourceReader(classLoader.getResourceAsStream("poems.txt"));
+		final String IDENTIFIER_ID = "ID";
 		final String IDENTIFIER_TITLE = "TITLE";
 		final String IDENTIFIER_AUTHOR = "AUTHOR";
 		final String IDENTIFIER_CONTENT = "CONTENT";
@@ -36,6 +37,8 @@ public final class PoetryLibrary extends Library{
 			pb.setContent(reader.getValue(IDENTIFIER_CONTENT));
 			if (pb.isComplete()) {
 				poems.add(pb.createPoem());
+			} else {
+				DLogger.warning("Parsed incomplete poem! (ID = " + reader.getValue(IDENTIFIER_ID) + ")");
 			}
 		}
 		
