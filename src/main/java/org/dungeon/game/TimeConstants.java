ac1093223a8af9647a37e77613099dfdb7191ba3
FRCRUADB <FRCRUADB@KMECLTqpopjNEHZ.kme.intern>
2024-06-25 13:36:57 +0200

    (v0.0.6) Implements healing through sleep

--- src/main/java/org/dungeon/game/TimeConstants.java
+++ src/main/java/org/dungeon/game/TimeConstants.java
@@ -5,7 +5,7 @@ public class TimeConstants {
 	public static final int WALK_SUCCESS = 200;
 	public static final int WALK_BLOCKED = 2;
 	
-	public static final int REST_COMPLETE = 36000;
+	public static final int HEAL_TEN_PERCENT = 3600;
 	
 	public static final int BATTLE_TURN_DURATION = 30;
 
