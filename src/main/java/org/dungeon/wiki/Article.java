28d131c3ce5497287d54406edfed8629a8768bde
FRCRUADB <FRCRUADB@KMECLTqpopjNEHZ.kme.intern>
2024-06-27 09:08:01 +0200

    (v0.0.7) Wiki draft

---
+++ src/main/java/org/dungeon/wiki/Article.java
@@ -0,0 +1,13 @@
+package main.java.org.dungeon.wiki;
+
+public class Article {
+	
+	public final String title;
+	public final String content;
+	
+	public Article(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+
+}
---
+++ src/main/java/org/dungeon/wiki/Wiki.java
@@ -0,0 +1,11 @@
+package main.java.org.dungeon.wiki;
+
+import main.java.org.dungeon.io.IO;
+
+public class Wiki {
+	
+	public void search(String searchArguments) {
+		IO.writeString("Wiki not yet implemented.");
+	}
+
+}
